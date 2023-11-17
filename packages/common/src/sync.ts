9eafd4c7ab9d1fef775679a7edf78f60d229ccd9
pedrogao <1312342604@qq.com>
2023-11-17 13:15:12 +0800

    fix: eslint rules

--- packages/common/src/sync.ts
+++ packages/common/src/sync.ts
@@ -36,6 +36,31 @@ import { JsonDecoder, JsonEncoder } from './json'
  */
 
 /**
+ * Yjs 核心定义了两种消息类型：
+ * • YjsSyncStep1：包含发送客户端的状态集。接收到后，客户端应该回复 YjsSyncStep2。
+ * • YjsSyncStep2：包含所有缺失的结构和完整的删除集。接收到后，客户端确信它接收到了远程客户端的所有信息。
+ *
+ * 在点对点网络中，你可能想引入一个 SyncDone 消息类型。双方都应该用 SyncStep1 初始化连接。当一个客户端接收到 SyncStep2 时，它应该回复 SyncDone。
+ * 当本地客户端接收到 SyncStep2 和 SyncDone 后，它确信已经与远程客户端同步。
+ *
+ * 在客户端-服务器模型中，你想以不同的方式处理这个问题：客户端应该用 SyncStep1 初始化连接。
+ * 当服务器接收到 SyncStep1 时，它应该立即回复 SyncStep2，紧接着是 SyncStep1。
+ * 客户端在接收到 SyncStep1 时回复 SyncStep2。
+ * 可选的是，服务器在接收到 SyncStep2 后可以发送一个 SyncDone，这样客户端就知道同步已经完成。
+ *
+ * 这个更复杂的同步模型有两个原因：
+ * 1. 这个协议可以容易地在 http 和 websockets 之上实现。
+ * 2. 服务器应该只回复请求，而不是发起它们。因此，必须由客户端发起同步。
+ *
+ * 消息的构建：
+ * [messageType : varUint, 消息定义..]
+ *
+ * 注意：消息不包括关于房间名称的信息。这必须由上层协议处理！
+ *
+ * stringify[messageType] 字符串化一个消息定义（messageType 已经从缓冲区读取）
+ */
+
+/**
  * Create a sync step 1 message based on the state of the current shared document.
  */
 export const writeSyncStep1 = (encoder: JsonEncoder, doc: Y.Doc) => {
